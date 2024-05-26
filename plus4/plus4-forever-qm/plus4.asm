30ce2f0e8b8ba03271b6b634fd88c790da6b189f
micro <litwr@yandex.ru>
2024-05-26 17:22:01 +0300

    plus4: forever-qm 1.19

--- plus4/plus4-forever-qm/plus4.asm
+++ plus4/plus4-forever-qm/plus4.asm
@@ -1,107 +1,97 @@
-                PAGE    255,132
+format MZ
 
-                INCLUDE CONST.MAC
+                INCLUDE 'fasm.mac'
+                INCLUDE '8501.mac'
+                INCLUDE 'adlib.mac'
+                INCLUDE 'const.mac'
 
-                .486
-SEG_DATA        SEGMENT PARA PUBLIC USE16
-                ASSUME  DS:SEG_DATA
-                INCLUDE DOS.DAT
-                INCLUDE MDAC.DAT
-                INCLUDE ROMFN.DAT
-                INCLUDE 8501.DAT
-                INCLUDE CDS.DAT
-                INCLUDE DEBUGGER.DAT
-                INCLUDE KBD.DAT
-                INCLUDE JOY.DAT
-                INCLUDE IO.DAT
-                INCLUDE TED.DAT
-                INCLUDE MENUS0.DAT
-                INCLUDE MENUS1.DAT
-                INCLUDE MENUS2.DAT
-                INCLUDE MENUS3.DAT
-                INCLUDE SOUND.DAT
-SEG_DATA        ENDS
+ENTRY SEG_CODE:START                    ;program entry point
+STACK 2000H                             ;stack size
+HEAP 0
 
-                INCLUDE HARDWARE.MAC
-                INCLUDE 8501.MAC
+SEGMENT         SEG_DATA USE16
+                INCLUDE 'dos.dat'
+                INCLUDE 'mdac.dat'
+                INCLUDE 'romfn.dat'
+                INCLUDE '8501.dat'
+                INCLUDE 'cds.dat'
+                INCLUDE 'debugger.dat'
+                INCLUDE 'kbd.dat'
+                INCLUDE 'joy.dat'
+                INCLUDE 'io.dat'
+                INCLUDE 'ted.dat'
+                INCLUDE 'menus0.dat'
+                INCLUDE 'menus1.dat'
+                INCLUDE 'menus2.dat'
+                INCLUDE 'menus3.dat'
+                INCLUDE 'sound.dat'
+                INCLUDE 'adlib.dat'
+                INCLUDE 'timer.dat'
 
-SEG_CODE        SEGMENT PARA PUBLIC USE16
-                ASSUME  CS:SEG_CODE
-                ORG     0
+SEGMENT         SEG_CODE USE16          ;main program segment
 START:          PUSH    DS              ;SET RETURN TO DOS ADDRESS
                 SUB     AX,AX
                 PUSH    AX
                 MOV     AX,SEG SEG_DATA ;INIT DS
                 MOV     DS,AX
-                MOV     RESET_SP,SP
+                MOV     [RESET_SP],SP
 
                 CALL    INITIBM
 
-RESET:          MOV     SP,RESET_SP
+RESET:          MOV     SP,[RESET_SP]
                 CALL    XRESET
-MAIN_ENTRY:     MOV     MAINJUMP,OFFSET MAINLOOP
+MAIN_ENTRY:     MOV     [MAINJUMP],MAINLOOP
 
 MAINLOOP:       CALL    TED
                 CALL    CPU8501
                 JMP     [MAINJUMP]
 
-END_EMU:        MOV     SP,RESET_SP
+END_EMU:        MOV     SP,[RESET_SP]
                 CALL    FINISHIBM
                 RETF
 
-                INCLUDE TIMER.COD
-                INCLUDE KBD.COD
-                INCLUDE JOY.COD
-                INCLUDE TED.COD
-                INCLUDE 8501.COD
-                INCLUDE CPU_IO.COD
-                INCLUDE CDS.COD
-                INCLUDE CSB.COD
-;                INCLUDE ACIA.INC
-;                INCLUDE C1551.INC
-;                INCLUDE CSB_FDD.INC
-;                INCLUDE CSB_PRT.INC
-                INCLUDE HARDWARE.COD
-                INCLUDE DEBUGGER.COD
-                INCLUDE MENUS0.COD
-                INCLUDE MENUS1.COD
-                INCLUDE MENUS2.COD
-                INCLUDE MENUS3.COD
-                INCLUDE ADLIB.COD
-SEG_CODE        ENDS
+                INCLUDE 'kbd.cod'
+                INCLUDE 'joy.cod'
+                INCLUDE 'ted.cod'
+                INCLUDE '8501.cod'
+                INCLUDE 'cpu_io.cod'
+                INCLUDE 'cds.cod'
+                INCLUDE 'csb.cod'
+;                INCLUDE 'acia.inc'
+;                INCLUDE 'c1551.inc'
+;                INCLUDE 'csb_fdd.inc'
+;                INCLUDE 'csb_prt.inc'
+                INCLUDE 'hardware.cod'
+                INCLUDE 'debugger.cod'
+                INCLUDE 'menus0.cod'
+                INCLUDE 'menus1.cod'
+                INCLUDE 'menus2.cod'
+                INCLUDE 'menus3.cod'
+                INCLUDE 'sound.cod'
+                INCLUDE 'adlib.cod'
+                INCLUDE 'timer.cod'
 
-SEG_S           SEGMENT PARA STACK USE16
-                DW      2*2048 DUP (7)
-SEG_S           ENDS
+SEGMENT         SEG_RAM USE16
+TIMES   32*1024 DW      0
 
-SEG_RAM         SEGMENT PARA PUBLIC USE16
-                DW      32*1024 DUP(0)
-SEG_RAM         ENDS
+SEGMENT         SEG_ROM USE16
+                FILE    '../BASIC.ROM'
+                FILE    '../KERNAL.ROM'
+                FILE    '../3P1.ROM'
 
-SEG_ROM         SEGMENT PARA PUBLIC USE16
-                INCLUDE ..\BASIC.INC
-                INCLUDE ..\KERNEL.INC
-                INCLUDE ..\3P1.INC
-SEG_ROM         ENDS
+SEGMENT         SEG_CART USE16
+TIMES   16*1024 DB      'F'             ;C1-LOW
+TIMES   16*1024 DB      'G'             ;C1-HIGH
+TIMES   16*1024 DB      'H'             ;C2-LOW
+TIMES   16*1024 DB      'I'             ;C2-HIGH
 
-SEG_CART        SEGMENT PARA PUBLIC USE16
-                DB      16*1024 DUP('F')        ;C1-LOW
-                DB      16*1024 DUP('G')        ;C1-HIGH
-                DB      16*1024 DUP('H')        ;C2-LOW
-                DB      16*1024 DUP('I')        ;C2-HIGH
-SEG_CART        ENDS
+SEGMENT         SEG_VRAM1 USE16
+TIMES   32*1024 DW      'V1'
 
-SEG_VRAM1       SEGMENT PARA PUBLIC USE16
-                DW      32*1024 DUP('1V')
-SEG_VRAM1       ENDS
+SEGMENT         SEG_VRAM2 USE16
+TIMES   4*1024  DW      'V2'
 
-SEG_VRAM2       SEGMENT PARA PUBLIC USE16
-                DW      4*1024 DUP('2V')
-SEG_VRAM2       ENDS
-
-SEG_AUX         SEGMENT PARA PUBLIC USE16
-                DB      8000 DUP(0)             ;DEBUGGER
-                DB      8000 DUP(0)             ;DATASETTE
-SEG_AUX         ENDS
-                END     START
+SEGMENT         SEG_AUX USE16
+TIMES   8000    DB      4               ;DEBUGGER
+TIMES   8000    DB      5               ;DATASETTE
 
