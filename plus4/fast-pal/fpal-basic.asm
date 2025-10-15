22cecce228fdf77cfa76411e1b5eb1cc6c6960a6
micro <litwr@yandex.ru>
2025-10-15 11:21:03 +0300

    Plus4: Fast PAL: a disk initialization + various updates

--- plus4/fast-pal/fpal-basic.asm
+++ plus4/fast-pal/fpal-basic.asm
@@ -33,7 +33,7 @@ irq1:
      lda #0
      sta $ff19
   endif
-     lda #<irq2
+.r6  lda #<irq2
      sta $314
    if VR
      tsx
@@ -43,7 +43,7 @@ irq1:
 .r3  sta .m2+2
 .r4  lda #>.p1
      sta $106,x
-     lda #<.p1
+.r5  lda #<.p1
      sta $105,x
 .m1  jmp 0
 .p1  php
@@ -76,7 +76,7 @@ irq2:
      sta $ff0b
      lda #$a3
      sta $ff0a
-     lda #<irq3
+.r1  lda #<irq3
      sta $314
      inc $ff09
      jmp $fcbe
@@ -92,7 +92,7 @@ irq3:
      sta $ff0b
      lda #$a2
      sta $ff0a
-     lda #<irq4
+.r1  lda #<irq4
      sta $314
      inc $ff09
      jmp $fcbe
@@ -109,7 +109,7 @@ irq4:
      sta $ff0b
      lda #$a2 + >PALPOS5
      sta $ff0a
-     lda #<irq5
+.r1  lda #<irq5
      sta $314
   if BLACKB
 .m2  lda #0
@@ -125,17 +125,72 @@ irq5:
      sta $ff0b
      lda #$a2
      sta $ff0a
-     lda #<irq1
+.r1  lda #<irq1
      sta $314
      inc $ff09
      jmp $fcbe
+
+acoff:
+     sei
+     lda #$e
+     ldx #$ce
+     bne acon.le
+
+acon:
+     sei
+.m1  lda #0
+.m2  ldx #0
+.le  sta $314
+     stx $315
+     cli
+     rts
 
      byte 0
 SOB
      byte 0,0
 
 start:
-     lda #$10
+     ldx #$10
+     lda $2002
+     bne .l4
+
+     ldx #3
+     clc
+     lda #$32
+     sta .m1+1
+     sta .m3+1
+     sta acon.m1+1
+     adc irq1.r6+1
+     sta irq1.r6+1
+     lda #$32
+     adc irq2.r1+1
+     sta irq2.r1+1
+     lda #$32
+     adc irq3.r1+1
+     sta irq3.r1+1
+     lda #$32
+     adc irq4.r1+1
+     sta irq4.r1+1
+     lda #$32
+     adc irq5.r1+1
+     sta irq5.r1+1
+  if BLACKB
+     lda #$32
+     adc irq1.r1+1
+     sta irq1.r1+1
+  endif  
+  if VR
+     lda #$32
+     adc irq1.r2+1
+     sta irq1.r2+1
+     lda #$32
+     sta irq1.r5+1
+     adc irq1.r3+1
+     sta irq1.r3+1
+  endif
+.l4  stx .m1+2
+     stx .m2+1
+     stx acon.m2+1
   if BLACKB
      sta irq1.r1+2
   endif
@@ -151,13 +206,13 @@ start:
      lda $2000  ;el1
      asl
      asl  ;sets C=0
-     sta $2002
+     sta $2003
      eor #$ff
      adc #PALPOS0+1  ;PALPOS0-4*el1
      sta irq5.me2+1
 
      lda $2000
-     adc $2002  ;C=1, sets C=0
+     adc $2003  ;C=1, sets C=0
      adc #NTSCPOS0-1  ;NTSCPOS0+5*el1
      sta irq1.me1+1
 
@@ -172,34 +227,83 @@ start:
      adc #NTSCPOS0
      sta irq3.me1+1  ;NTSCPOS0+5*el2
 
+     jsr JPRIMM
+     byte "SYS",0
+
+     clc
+     lda .m1+1
+     adc #<acoff
+     tax
+     lda .m1+2
+     jsr INT2STR
+     jsr JPRIMM
+     byte " - THROTTLE OFF",13,0     
+
+     jsr JPRIMM
+     byte "SYS",0
+
+     clc
+     lda .m1+1
+     adc #<acon
+     tax
+     lda .m1+2
+     jsr INT2STR
+     jsr JPRIMM
+     byte " - THROTTLE ON",13,0
      ldx #0
 .l1  lda $1300,x
-     sta $1000,x
+.m1  sta $1000,x
      inx
      cpx #<start
      bne .l1
 
-     sei
-     lda #<SOB
+     ldx $2002
+     beq .l2
+
+     dex
+     beq .l5
+
+     lda #0
+     sta $4001
+     sta $4002
+     sta $4000
+     lda #$40
+     bne .l6
+
+.l5  lda #<SOB
      sta $2b
      lda #>SOB-3
      sta $2c
-     lda #<start
-     sta $2d
-     sta $2f
-     sta $31
      lda #>start-3
      sta $2e
      sta $30
      sta $32
+     lda #<start
+     bne .l3
 
+.l2  lda #0
+     sta $1001
+     sta $1002
+     lda #$10
+.l6  sta $2c
+     sta $2e
+     sta $30
+     sta $32
+     lda #1
+     sta $2b
+     lda #3
+.l3  sta $2d
+     sta $2f
+     sta $31
+
+     sei
      lda irq5.me2+1
      sta $ff0b
      ;lda #$a2
      ;sta $ff0a
-     lda #<irq1
+.m3  lda #<irq1
      sta $314
-     lda #>irq1-3
+.m2  lda #0
      sta $315
      cli
      sec
--- plus4/fast-pal/fpal-basic.bas
+++ plus4/fast-pal/fpal-basic.bas
@@ -1,9 +1,14 @@
 10 print"** ntsc throttle v1 by litwr, 2025"
-20 print"use ecmeter to find the best values for your monitor"
+20 print"use ecmeter to find the best values for"spc(29)"your monitor"
 30 if peek(65287)and64 then print"this only works on pal computers":end
 40 input"extra lines before the vsync (1-11)";a:ifa<1ora>11then40
 50 input"extra lines after the vsync (3-12)";b:ifb<3orb>12then50
-60 printusing"this sets about #.##% acceleration and  distorts about ##.##% of the raster";.475*(a+b-3)+.16,(a+b)*50/39
+60 printusing"this sets about #.##% acceleration and";.475*(a+b-3)+.16
+65 printspc(5);:printusing"distorts about ##.##% of the raster";(a+b)*50/39
 70 print"proceed?(y/n)":getkeya$:ifa$="n"goto40:else:ifa$<>"y"goto70
-80 print"do not use split screen modes!"
-90 poke8192,a:poke8193,b:clr:sys1111
+80 print"where put the code:":print"1 @$332 (cassette buffer)"
+90 print"2 @$1000 (no graphics)"
+100 print"3 @$1000 (graphics on)"
+110 inputc:ifc<1orc>3then80
+120 print"do not use split screen modes!"
+130 poke8192,a:poke8193,b:poke8194,c-1:clr:sys1111
